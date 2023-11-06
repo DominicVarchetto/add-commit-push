2b7144d90ce27245cff8680259eb447842bcf192
Dominic Varchetto <varchettodominic20@gmail.com>
2023-11-05 20:58:12 -0600

    Changed Files

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,1 +1,11 @@
+import os
+
 print ("Add Commit Push")
+print ("\nGit status")
+os.system ("git status")
+print ("\ngit add -A")
+os.system ("git add -A")
+print ('git commit -m "Changed Files"')
+os.system ('git commit -m "Changed Files"')
+print ("git push")
+os.system ("git push")
